a7f124becf2dbfd3abda2b57194fa05aba5e704c
Rémy Lavainne <remy.lavainne@ensimag.grenoble-inp.fr>
2019-02-22 16:45:35 +0100

    filtrage Gaussien avec Segmentation fault

--- code_src/filtrage_gaussien.c
+++ code_src/filtrage_gaussien.c
@@ -1,23 +1,24 @@
-#include "conv_freq.h"
+#include <math.h>
+#include "filtrage_gaussien.h"
+#include "pgm.h"
 
 
-unsigned char** filtrageGaussien( unsigned char** sortie,  unsigned char** entree, int nl, int nc) { 
+unsigned char** filtrageGaussien(double** entree, int nl, int nc) { 
 
     int i,j;
     int oldnl, oldnc;
     int sigma = 5;
-    unsigned char** im7, ** im4, ** im5, ** im6, **im8, **im9, **im10, **im11;
-    if (sortie==NULL) sortie=alloue_image(nl,nc);
-    unsigned double** filtreGaussienReel = alloue_image_double(nl,nc);
-    unsigned double** filtreGaussienImag = alloue_image_double(nl,nc);
+    double** im7, ** im4, ** im5, ** im6, **im8, **im9, **im10, **im11;
+    double** filtreGaussienReel = alloue_image_double(nl,nc);
+    double** filtreGaussienImag = alloue_image_double(nl,nc);
     for(i=0; i<nl; i++){
         for(j=0; j<nc; j++){
-            filtreGaussienReel[i][j]=exp(-2*pi*pi*sigma*sigma*(((i-(nl/2))/nl)**2+((j-(nc/2))/nc)**2));
+            filtreGaussienReel[i][j]=exp(-2*M_PI*M_PI*sigma*sigma*(pow((i-(nl/2))/nl,2)+pow((j-(nc/2))/nc,2)));
             filtreGaussienImag[i][j]=0;
         }
     }
     oldnl = nl; oldnc = nc;
-    im7 = padimdforfft(im3, &nl, &nc); 
+    im7 = padimdforfft(entree, &nl, &nc); 
     im4 = alloue_image_double(nl,nc); im5 = alloue_image_double(nl,nc);
     im6 = alloue_image_double(nl,nc); im8 = alloue_image_double(nl,nc);
     im9 = alloue_image_double(nl,nc);
@@ -26,16 +27,15 @@ unsigned char** filtrageGaussien( unsigned char** sortie,  unsigned char** entre
     multiplication(im8, im9, filtreGaussienReel, filtreGaussienImag, im10, im11, nl, nc);
     fftshift(im10, im11, im5, im6, nl, nc); //Reprise des anciens images
     ifft(im5, im6, im8, im9, nl, nc);
-    for(i=0; i<nl; i++){
-        for(j=0; j<nc; j++){
-            sortie[i][j] = im8[i][j];
-        }
-    }
-    return sortie;
+    return imdouble2char(im8, nl, nc);
 }
 
-void multiplication (unsigned double** entreReel1, unsigned double** entreImag1, unsigned double** entreReel2, unsigned double** entreImag2, unsigned double** sortieReel, unsigned double** sortieImag, int nl, int nc){
-    if (sortie==NULL) sortieReel=alloue_image_double(nl,nc);
+void multiplication (double** entreReel1, double** entreImag1,
+        double** entreReel2, double** entreImag2, 
+        double** sortieReel, double** sortieImag, 
+        int nl, int nc){
+    int i,j;
+    if (sortieReel==NULL) sortieReel=alloue_image_double(nl,nc);
     if (sortieImag == NULL) sortieImag = alloue_image_double(nl,nc);
     for(i=0;i<nl;i++){
         for(j=0;j<nc;j++){
@@ -48,14 +48,15 @@ void multiplication (unsigned double** entreReel1, unsigned double** entreImag1,
 main (int ac, char **av) {  /* av[1] contient le nom de l'image, av[2] le nom du resultat . */
   int nb,nl,nc, oldnl,oldnc;
   unsigned char **im2=NULL,** im1=NULL;
-  double** im4,** im5, ** im6, ** im7, **im8, **im9,**im10;
+  //double** im4,** im5, ** im6, ** im7, **im8, **im9,**im10;
 
   if (ac < 2) {printf("Usage : %s entree sortie \n",av[0]); exit(1); }
 	/* Lecture d'une image pgm dont le nom est passe sur la ligne de commande */
   im1=lectureimagepgm(av[1],&nl,&nc);
   if (im1==NULL)  { puts("Lecture image impossible"); exit(1); }
 	/* Calcul de son inverse video */
-  im2=inverse(NULL,im1,nl,nc);
+  double**im3=imuchar2double(im1,nl,nc);
+  im2=filtrageGaussien(im3, nl, nc);
 	/* Sauvegarde dans un fichier dont le nom est passe sur la ligne de commande */
   ecritureimagepgm(av[2],im2,nl,nc);
 }
--- code_src/filtrage_gaussien.h
+++ code_src/filtrage_gaussien.h
@@ -2,7 +2,12 @@
 #define CONV_FREQ
 #include "pgm.h"
 
-void conv_freq(char* filename, double sigma);
+unsigned char** filtrageGaussien(double** entree, int nl, int nc);
+
+void multiplication (double** entreReel1, double** entreImag1,
+        double** entreReel2, double** entreImag2, 
+        double** sortieReel, double** sortieImag, 
+        int nl, int nc);
 
 #endif
 
